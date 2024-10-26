c3c4b6a45b259c9b9b7d59bf9be8a3b876ec4bae
Daniel Marín <info@codigobeta.es>
2024-10-26 09:37:15 +0200

    ajuste js

--- wp-content/themes/twentytwentyone/footer.php
+++ wp-content/themes/twentytwentyone/footer.php
@@ -76,23 +76,23 @@
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
 	<!-- Slick slider 1.8.1 -->
-	<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js" async></script>
+	<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
 	<!-- lazy load -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.js"></script>
 
 	<!-- jquery ui (calendar) -->
-	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" async></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 
 	<!-- masonry --> 
 	<script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.js"></script>
 	<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.js"></script>
 
 	<!-- fancybox -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js" async></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
 
 	<!-- aos animations --> 
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js" async></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
 
 	
 </div><!-- #page -->
